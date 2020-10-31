3dd8b40156ef6aeaaf3715d9e5ecfd26f23f9098
ritachiou <sdi1700192@di.uoa.gr>
2020-11-01 01:01:05 +0200

    Added dots

--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 int main(){
-  printf("Got to my local dir");
-  printf("Back to github");
-  printf("Another test");
+  printf("Got to my local dir.");
+  printf("Back to github.");
+  printf("Another test.");
   printf("Will this pull mess up my changes?");
   return 0;
 }
