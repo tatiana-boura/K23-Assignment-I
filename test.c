67f0c56fcc2aa5f0b8c0492bb8ce182d1a56c058
ritachiou <73245577+ritachiou@users.noreply.github.com>
2020-11-01 02:01:15 +0200

    Delete all and keep "Test"

--- test.c
+++ test.c
@@ -1,7 +1,4 @@
 int main(){
-  printf("Got to my local dir.");
-  printf("Back to github.");
-  printf("Another test.");
-  printf("Will this pull mess up my changes?");
+  printf("Test");
   return 0;
 }
