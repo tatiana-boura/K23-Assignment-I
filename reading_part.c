5b1e5cdd0fe05e9993a87d452881a7a515f082c7
kateCh0us <katerina.houssia1999@gmail.com>
2020-11-05 23:20:24 +0200

    json file counter function/changed names/cleaner code?

--- reading_part.c
+++ reading_part.c
@@ -34,25 +34,95 @@ extra notes:
 #include <unistd.h>
 #include <dirent.h>
 
+//function to return #.json files in given directory
+int count_json(char* given_directory){
+	printf("given_dir: %s\n", given_directory);
+
+	DIR* main_dir;
+	DIR* sub_dir;  
+	struct dirent* main_dir_entry;
+	struct dirent* sub_dir_entry;
+	FILE* json_file;
+	unsigned int count=0;
+	
+	//create string to store, for each main directory entry, access path 
+	char* path = calloc(200, sizeof(char));
+	assert( path != NULL );
+	memset(path ,'\0' , 200);
+	strcpy(path, given_directory);
+	strcat(path, "/");        //current path: "<given_directory>/"
+
+	main_dir = opendir(given_directory);
+	if (main_dir == NULL){ 
+		perror("Unable to read directory"); 
+		exit(-1); 
+	}
+	//main directory opened successfully
+
+	//open each directory entry from the main directory 
+	while ((main_dir_entry = readdir(main_dir)) != NULL){ 
+        if( (strcmp(main_dir_entry->d_name,".") != 0) && (strcmp(main_dir_entry->d_name,"..")!=0) ){ 
+        	strcat(path, main_dir_entry->d_name);
+        	sub_dir = opendir(path);      //current path: "<given_directory>/<main_dir_entry->d_name>"
+			if (sub_dir == NULL){ 
+				perror("Unable to read directory"); 
+				exit(-1); 
+			}
+			//sub directory opened successfully
+			while ((sub_dir_entry = readdir(sub_dir)) != NULL){ 
+        		if( (strcmp(sub_dir_entry->d_name,".") != 0) && (strcmp(sub_dir_entry->d_name,"..")!=0) ){
+        			count++;
+        			//printf("%d) %s\n", count, sub_dir_entry->d_name ); 
+        		}
+        	}
+        	closedir(sub_dir);
+        	
+        	memset(path ,'\0' , 200);
+			strcpy(path, given_directory);
+			strcat(path, "/");        //current path: "<given_directory>/"
+        }
+    }
+    closedir(main_dir);
+    free(path);
+    return count;
+}
+
+
+/*
+Name changes
+============
+Dataset_X --> main_dir
+dirX -->  sub_dir
+json_spec_file --> json_file
+entry--> main_dir_entry
+sub_entry--> sub_dir_entry
+*/
+
+
 int main(int argc, char* argv[]){
-	//char* dataset_x;
+	//char* main_dir;
 	int error;
-	DIR* Dataset_X;       // [no inspo for the name] 
-	struct dirent* entry;
-	struct dirent* sub_entry;
-	DIR* dirX; //sub-directory of Dataset_X [no inspo for this name either]
-	FILE* json_spec_file;
+	DIR* main_dir;       // [no inspo for the name] 
+	struct dirent* main_dir_entry;
+	struct dirent* sub_dir_entry;
+	DIR* sub_dir; //sub-directory of main_dir [no inspo for this name either]
+	FILE* json_file;
 	char* buff = calloc(200,sizeof(char));
 
+	int num = count_json(argv[1]);
+	printf("num = %d\n", num);
+
+	//for later..
+	//unsigned int hash_size = num_of_dir;
+    //hashTable* hash = createHT(hash_size); 
 
 	//open directory 2013_camera_specs
-	Dataset_X = opendir(argv[1]);
-	if(Dataset_X == NULL){
+	main_dir = opendir(argv[1]);
+	if(main_dir == NULL){
 		perror("Unable to read directory");
-		return 1;
-	}else{
-		perror("Directory opend successfully!\n");		
+		exit(-1); 
 	}
+	//main directory opened successfully!
 
 	//dirpath: sting to built and store the pathname to each camera spec directory
 	char* dirpath = calloc(200,sizeof(char));  
@@ -66,55 +136,53 @@ int main(int argc, char* argv[]){
 	assert( json_path != NULL );
 	memset(json_path ,'\0' , 200); 
 
-	int dirs=0; //num of files in Dataset_X --> 2013_camera_specs
-	while( entry = readdir(Dataset_X) ){  //check each entry in the directory
-		
-		if((strcmp(entry->d_name,".")==0) || (strcmp(entry->d_name,"..")==0)){
-			//do not open current & parent dir again
-			//printf("Not interested to . or ..\n");
-		}else{
+	int dirs=0; //num of files in main_dir --> 2013_camera_specs
+	while( main_dir_entry = readdir(main_dir) ){  //check each entry in the directory
+		//ignore open current & parent dir
+		if((strcmp(main_dir_entry->d_name,".")!=0) && (strcmp(main_dir_entry->d_name,"..")!=0)){
 			dirs++;  //count #web dirs
-			printf("SUBDIR %d: %s\n", dirs, entry->d_name);
+			printf("SUBDIR %d: %s\n", dirs, main_dir_entry->d_name);
 
 			//open web-directory entry
-			strcat(dirpath, entry->d_name); //current dirpath: "2013_camera_specs/<entry->d_name>"
-			dirX = opendir(dirpath);
-			if(dirX == NULL){
+			strcat(dirpath, main_dir_entry->d_name); //current dirpath: "2013_camera_specs/<entry->d_name>"
+			sub_dir = opendir(dirpath);
+			if(sub_dir == NULL){
 				printf("Unable to read sub-directory\n");
-				return 1;
-			}else{
-				//printf("Sub-directory opend successfully!\n");
-				printf("Lets read some Json stuff!\n");
-				while( sub_entry = readdir(dirX) ){
-					if((strcmp(sub_entry->d_name,".")==0) || (strcmp(sub_entry->d_name,"..")==0)){
-						//do not open current & parent dir again
-						//printf("Not interested to . or ..\n");
-					}else{
-						//printf("\tjsonFILE: %s\n", sub_entry->d_name);  // <------------WORKS :D
+				exit(-1); 
+			}
+
+			//printf("Sub-directory opend successfully!\n");
+			printf("Lets read some Json stuff!\n");
+			while( sub_dir_entry = readdir(sub_dir) ){
+				//ignore current & parent dir
+				if((strcmp(sub_dir_entry->d_name,".")!=0) && (strcmp(sub_dir_entry->d_name,"..")!=0)){ 
+					//printf("\tjsonFILE: %s\n", sub_dir_entry->d_name);  // <------------WORKS :D
 						
-						//create path to json file
-						strcat(json_path,dirpath);
-						strcat(json_path,"/");
-						strcat(json_path,sub_entry->d_name);
+					//create path to json file
+					strcat(json_path,dirpath);
+					strcat(json_path,"/");
+					strcat(json_path,sub_dir_entry->d_name);
 
-						//open json file and print contents
-						json_spec_file = fopen(json_path, "r");
-						if(json_spec_file == NULL){
-							perror("Unable to open file :(");
-							return 1;
-						}else{
-							//printf("Oh!It opened!\n");
-							while( fgets(buff, 200, json_spec_file) != NULL ){
-								//puts(buff);                                    // <------------WORKS :D
-							}
-						}
-						fclose(json_spec_file);
-
-						memset(json_path ,'\0' , 200);
+					//open json file and print contents
+					json_file = fopen(json_path, "r");
+					if(json_file == NULL){
+						perror("Unable to open file :(");
+						exit(-1);
+					} 
+					//printf("Oh!It opened!\n");
+					/*
+					GONNA ADD STUFF - to 'break' .json content into tupples
+					*/
+					while( fgets(buff, 200, json_file) != NULL ){
+						//puts(buff);                                    // <------------WORKS :D
 					}
-				}	
+					fclose(json_file);
+
+					memset(json_path ,'\0' , 200);
+				}
 			}	
-			closedir(dirX);
+			
+			closedir(sub_dir);
 		}
 
 		memset(dirpath ,'\0' , 200);   
@@ -122,26 +190,7 @@ int main(int argc, char* argv[]){
 		strcat(dirpath, "/");
 	}
 
-	closedir(Dataset_X);
+	closedir(main_dir);
 	return 0;
 }
 
-/*
-Testing code that finds the sum of json files to create hash table (use size accordingly)
-//Get number of subdirectories (apart from /. and /..)
-    //open given directory
-    struct dirent *dp;
-    DIR *d;
-    //unable to open directory stream
-    if (!(d= opendir("test"))) { perror("opendir"); exit(-1); }
-
-    unsigned int num_of_dir=0;
-    while ((dp = readdir(d)) != NULL) 
-        if(!((!(strcmp(dp->d_name,".")))||(!(strcmp(dp->d_name,".."))))) num_of_dir++;
-    closedir(d);
-    rewinddir(d);
-
-    unsigned int hash_size = num_of_dir;
-
-    hashTable* hash = createHT(hash_size); 
-*/
