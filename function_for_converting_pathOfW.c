9afa8b50724cdbdb72ed5da3e0296f1fbfbf3097
Tatiana Boura <tat.boura@gmail.com>
2020-11-05 23:17:29 +0200

    made the function for rita

---
+++ function_for_converting_pathOfW.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#define STRSIZE 50
+
+char* convertPath(char* _path_ ){
+
+	char* adjustedPath = calloc(strlen(_path_)+1-1,sizeof(char)); assert( adjustedPath != NULL);
+	bool foundFirstTime=true;
+
+	unsigned int j=0;
+
+	for( unsigned int i=0; _path_[i] != '\0'; i++ )
+
+		if( _path_[i]=='/' ){
+			if(foundFirstTime){
+				foundFirstTime=false;
+				adjustedPath[j++]='/';
+			}
+		}else
+			adjustedPath[j++]=_path_[i];
+		
+
+	adjustedPath[j]='\0';
+
+	return adjustedPath;
+}
+
+
+int main(void){
+
+	char* strNew;
+	char* str = calloc(STRSIZE,sizeof(char)); assert(str!=NULL);
+	strcpy(str,"www.ebay.com//1");
+
+	strNew=convertPath(str);
+
+	printf("%s\n",strNew );
+
+	free(strNew); strNew=NULL;
+	free(str); str=NULL;
+
+	return 0;
+}
