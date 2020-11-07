b49a7d4d2ad9e32cf8a7c5d2a2298a590fcf6439
ritachiou <sdi1700192@di.uoa.gr>
2020-11-07 11:33:11 +0200

    Add datasets

---
+++ readfromW.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+	FILE* dataset_matches;
+	int error;
+	char* buff = calloc(200,sizeof(char));
+
+	//open matches dataset
+    dataset_matches = fopen(argv[1], "r");
+    if (dataset_matches == NULL){ perror("Unable to read directory"); exit(-1);  }
+    printf("File %s opened successfully.\n",argv[1]);
+    
+    
+    
+    
+    
+    
+    return 0;
+}
