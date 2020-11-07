dfc8dd7434783ad8018743ecdf9a6b12dd3a9553
ritachiou <sdi1700192@di.uoa.gr>
2020-11-07 11:34:14 +0200

    Open file with matches

--- readfromW.c
+++ readfromW.c
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]){
 	int error;
 	char* buff = calloc(200,sizeof(char));
 
-	//open matches dataset
+	//open dataset file with matches
     dataset_matches = fopen(argv[1], "r");
     if (dataset_matches == NULL){ perror("Unable to read directory"); exit(-1);  }
     printf("File %s opened successfully.\n",argv[1]);
