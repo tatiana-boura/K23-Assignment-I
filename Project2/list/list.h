df560e6952ea7617a2aadee411286f665e155bf9
Tatiana Boura <tat.boura@gmail.com>
2020-11-28 17:24:57 +0200

    separated node, new output

---
+++ Project2/list/list.h
@@ -0,0 +1,24 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "../hash_table/bucket.h"
+#include "node.h"
+
+node* appendList(node* n, void* data);
+void destroyList(node* n, unsigned int* size, bool del, void(*deleteData)(void*,void*));
+node* mergeTwoLists(node* n1, node* n2);
+void destroyListOfTuples(node* n, void(*deleteData)(void*));
+void destroyListOfStrings(node* n, bool destroyDataAsWell);
+
+void printList(node* n, void(*printData)(void*));
+
+bool addrFoundinList(node* n,  void* addr );
+
+void makeOutputFileList(node* n,FILE* outputFile);
+
+#endif
---
+++ Project2/list/node.h
@@ -0,0 +1,9 @@
+#ifndef __NODE_H__
+#define __NODE_H__
+
+typedef struct node{
+    void* data;
+    struct node* next;
+}node;
+
+#endif
