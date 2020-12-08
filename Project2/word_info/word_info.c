9b3695a657e44dcb368f10e1c2d404d67cd74994
ritachiou <sdi1700192@di.uoa.gr>
2020-12-08 21:14:31 +0200

    Add init, delete, print for wordInfo

---
+++ Project2/word_info/word_info.c
@@ -0,0 +1,33 @@
+#include "word_info.h"
+
+#define NAME_SIZE 100
+#define VALUE_SIZE 300
+
+void wordInfoInitialization(wordInfo* w, char* _word_){ 
+
+	/*w = calloc(1,sizeof(wordInfo));
+	assert(w!=NULL);*/
+
+	w->word=_word_;
+	w->count = 0;
+
+	return;
+}
+
+void wordInfoDeletion(wordInfo* w){ 
+
+	free(w->word); w->word=NULL;
+	free(w); w=NULL;
+
+	return;
+}
+
+void printWordInfo(wordInfo* w){
+
+	if(w==NULL){ printf("NULL wordInfo\n"); return;}
+
+	printf("[word] = [%s],[count] = %d\n",w->word,w->count);
+	printList(t->propertyValueList,NULL);
+
+	return;
+}
