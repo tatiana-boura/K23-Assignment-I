965878c8bf24eb2acf14b76b547f6eca1d5182f9
Tatiana Boura <tat.boura@gmail.com>
2020-12-10 01:08:33 +0200

    change mine tester according to changes

--- Project2/hash_table/taniaTest.c
+++ Project2/hash_table/taniaTest.c
@@ -32,22 +32,22 @@ int main(int argc, char* argv[]){
     /* WordInfoListA gets w1-w6  */
     node* wordInfoListA=NULL; 
     
-    addToVoc(&vocabulary,w1);
+    addToVoc(&vocabulary,w1, wordInfoListA);
     addToWordInfoList(&wordInfoListA,w1); 
    
-    addToVoc(&vocabulary,w2);
+    addToVoc(&vocabulary,w2,wordInfoListA);
     addToWordInfoList(&wordInfoListA,w2); 
    
-    addToVoc(&vocabulary,w3);
+    addToVoc(&vocabulary,w3,wordInfoListA);
     addToWordInfoList(&wordInfoListA,w3); 
   
-    addToVoc(&vocabulary,w4);
+    addToVoc(&vocabulary,w4,wordInfoListA);
     addToWordInfoList(&wordInfoListA,w4);
     
-    addToVoc(&vocabulary,w5);
+    addToVoc(&vocabulary,w5,wordInfoListA);
     addToWordInfoList(&wordInfoListA,w5); 
    
-    addToVoc(&vocabulary,w6);
+    addToVoc(&vocabulary,w6,wordInfoListA);
     addToWordInfoList(&wordInfoListA,w6); 
 
     /* Second sentence / .json */
@@ -61,29 +61,29 @@ int main(int argc, char* argv[]){
     node* wordInfoListB=NULL; 
     
     
-    addToVoc(&vocabulary,w7);
+    addToVoc(&vocabulary,w7,wordInfoListB);
     addToWordInfoList(&wordInfoListB,w7); 
 
     
-    addToVoc(&vocabulary,w8);
+    addToVoc(&vocabulary,w8,wordInfoListB);
     addToWordInfoList(&wordInfoListB,w8); 
 
    
-    addToVoc(&vocabulary,w9);
+    addToVoc(&vocabulary,w9,wordInfoListB);
     addToWordInfoList(&wordInfoListB,w9); 
 
     
-    addToVoc(&vocabulary,w10);
+    addToVoc(&vocabulary,w10,wordInfoListB);
     addToWordInfoList(&wordInfoListB,w10);
 
     
-    addToVoc(&vocabulary,w11);
+    addToVoc(&vocabulary,w11,wordInfoListB);
     addToWordInfoList(&wordInfoListB,w11); 
 
     printList(vocabulary,(void*)printWordInfo);printf("\n");printf("\n");
 
     char* sA = calloc(100,sizeof(char)); strcpy(sA,"book");
-	char* sB = calloc(100,sizeof(char)); strcpy(sB,"thanks");
+    char* sB = calloc(100,sizeof(char)); strcpy(sB,"thanks");
 
    
 
