88f2d63980d194b1a7845d61ca81b4dd40e3cfbb
ritachiou <sdi1700192@di.uoa.gr>
2020-12-09 05:15:39 +0200

    Add functions to insert words in lists with counts and no duplicates

---
+++ Project2/vocabulary/vocabulary.h
@@ -0,0 +1,5 @@
+#include "../hash_table/hash_table.h"
+
+void addWordInfoToList(node** list,wordInfo* _word_info_);
+void addToWordInfoList(node** word_info_list,char* _word_);
+void addToVoc(node** voc,char* _word_);
