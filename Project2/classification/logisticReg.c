2f370dd87b3e9d695a2168be2856de81092d104d
Tatiana Boura <tat.boura@gmail.com>
2020-12-12 19:15:28 +0200

    fix error

--- Project2/classification/logisticReg.c
+++ Project2/classification/logisticReg.c
@@ -20,21 +20,27 @@ float hypothesis(float* x_i, float* w, float bias, unsigned int r){
 	return sigmoid(innerProduct( x_i, w, r) + bias);
 }
 
-bool* predict( float** x_test, float* y_test, float* w, float bias, unsigned int m, unsigned int r ){
+bool* predict( float** x_test, float* y_test, float* w, float bias, unsigned int n, unsigned int r ){
 	// predict class of x_test
 	/* return a boolean array where: 
 	   true -- classified correctly, false -- not classified correctly */
 
-	bool* correctClass = calloc(m,sizeof(bool)); assert( correctClass != NULL );
+	bool* correctClass = calloc(n,sizeof(bool)); assert( correctClass != NULL );
 
-	for( unsigned int i=0; i<m; i++ ){
-		correctClass[i] = (hypothesis(x_test[i], w, bias, r) == y_test[i]);
+	for( unsigned int i=0; i<n; i++ ){
+
+		bool class;
+
+		if( hypothesis(x_test[i], w, bias, r) <= 0.5 ) class = 0;
+		else class = 1;
+
+		correctClass[i] = (class == y_test[i]);
 	}
 
 	return correctClass;
 }
 
-void gradient_descent(float** x_train, float* y_train, float* w, float* bias, unsigned int m, unsigned int r, float eta, float epsilon){
+void gradient_descent(float** x_train, float* y_train, float* w, float* bias, unsigned int n, unsigned int r, float eta, float epsilon){
 	// train the model
 	float _hypothesis_, sum_weights;
 	float sum_bias;
@@ -50,7 +56,7 @@ void gradient_descent(float** x_train, float* y_train, float* w, float* bias, un
 
 	    	sum_weights = 0.0;
 	    	// for every observation
-		    for (unsigned int i = 0; i < m; i++){
+		    for (unsigned int i = 0; i < n; i++){
 		    	// compute the predicted value
 			    _hypothesis_ = hypothesis(x_train[i], w, (*bias), r);
 			    // update d/dw_j J(w,b) or bias
@@ -60,8 +66,8 @@ void gradient_descent(float** x_train, float* y_train, float* w, float* bias, un
 			    	sum_bias += (_hypothesis_- y_train[i]);
 		    }
 
-		    // J_weight/bias[j]/m ?????? 
-		    // I know that the /m is needed but they don't tell us so
+		    // J_weight/bias[j]/n ?????? 
+		    // I know that the /n is needed but they don't tell us so
 		    J_weight[j] = sum_weights;
 		    if( j==r ) J_bias = sum_bias;
 		}  
@@ -71,7 +77,7 @@ void gradient_descent(float** x_train, float* y_train, float* w, float* bias, un
 		*bias = *bias - eta*J_bias;
 		// update other weights
 		for( unsigned int j = 0; j < r; j++ )
-		    w[j] = w[j] - eta*J_weight[j];
+		    w[j] = w[j] - eta*J_weight[j];  
 		
 	}
 
