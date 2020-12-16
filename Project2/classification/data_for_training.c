c202bb72c5927b05ca77170ab6503eb926b23654
ritachiou <sdi1700192@di.uoa.gr>
2020-12-16 17:42:09 +0200

    Add storeAbsDifference and update createTFIDFarray to store differences (leaks)

--- Project2/classification/data_for_training.c
+++ Project2/classification/data_for_training.c
@@ -1,6 +1,8 @@
 #include "./data_for_training.h"
 
-void createTFIDFarray(float*** array,hashTable* ht,unsigned int bucketSize, unsigned int vocabSize,unsigned int* n){
+node* visitedList=NULL;
+
+void createTFIDFarray(float*** x_array,unsigned int** y_array,hashTable* ht,unsigned int bucketSize, unsigned int vocabSize,unsigned int* n){
 	*n=0;	//n:= number of absolute differences
 	
 	// go through every entry in the hash and make the bow vector
@@ -14,24 +16,98 @@ void createTFIDFarray(float*** array,hashTable* ht,unsigned int bucketSize, unsi
             while(temp!=NULL){
                 bucketEntry** entryTable = temp->data;
                 // and every entry of the bucket
-                for(unsigned int j = 0;j<numOfEntries;j++){
-                    if(entryTable[j]!=NULL){
-						(*array)[(*n)] = calloc(vocabSize,sizeof(float));
-						// FOR NOW pass every element of tfidf vector in hash to array
-                        for( unsigned int k = 0; k<vocabSize; k++ ){
-							(*array)[(*n)][k] = entryTable[j]->tfidf[k];
-							printf("%f\n",(*array)[(*n)][k]);
-                        }
-						if((j+1)!=numOfEntries){
-							(*n)++;
-							(*array) = realloc((*array),((*n)+1)*sizeof(float*));
-						}
-                    }
+                for(unsigned int j = 0;j<numOfEntries && entryTable[j]!=NULL;j++){
+                    if( !addrFoundinList(visitedList,entryTable[j]->clique)){
+                        visitedList=appendList(visitedList,entryTable[j]->clique);
+                        storeAbsDifference(entryTable[j], x_array, y_array, vocabSize, n);
+                    }else entryTable[j]->clique=NULL;
                 }
                 temp=temp->next;
             }
-
         } 
 	}
+    // we don't need anymore to keep list of visited cliques
+    destroyListOfStrings(visitedList,false); visitedList=NULL;
+}
 
+
+void storeAbsDifference(bucketEntry* entryTable_j,float*** x_array,unsigned int** y_array,unsigned int vocabSize, unsigned int* n){
+    /* This function works as follows: let's say we have the example,
+    clique is a b c d and its notClique is f e. The function stores the differences in x_array 
+    and their labels in y_array:
+    first it check if the notClique has been previously visited as a clique (if so the notClique
+    elements will not be paired), then the function
+    takes a and pairs it with the rest of the clique-- |a-b|, |a-c|, |a-d| (y_array: 1) and the 
+    notClique -- |a-f|, |a-e| (y_array: 0)
+    then takes b and pairs it with what follows of the clique-- |b-c|, |b-d| (y_array: 1) and all 
+    of the notClique-- |b-f|, |b-e| (y_array: 0)
+    then takes c-- |c-d| and |c-f|, |c-e| and at last reaches d that has no followed element in clique
+    and just pairs with |d-f|, |d-e|
+    */
+
+    node* outterTemp=entryTable_j->clique;
+    node* innerTemp; node* otherTemp;
+    bool revisitedNotClique = false;
+    if(addrFoundinList(visitedList,entryTable_j->notClique))
+        revisitedNotClique = true;
+
+    // takes an item
+    int times=0;
+    while( outterTemp!=NULL ){
+        printf("Outter %d\n",times);
+        // and begins printing it with the item that follow
+        innerTemp=outterTemp->next;
+        otherTemp=entryTable_j->notClique;
+
+        while( innerTemp!= NULL ){ 
+            // clique is a list of pointers to bucketEntries
+            bucketEntry* inner = innerTemp->data;
+            bucketEntry* outter = outterTemp->data;
+            // Difference between current (outter) and the rest of the clique
+            //allocate memory in arrays for difference
+            (*x_array)[(*n)] = calloc(vocabSize,sizeof(float));//n:= number of absolute differences
+            for( unsigned int k = 0; k<vocabSize; k++ ){
+                //calculate difference |outter->tfidf-inner->tfidf| 
+                float abs_dif = abs(outter->tfidf[k]-inner->tfidf[k]);
+                //add to x_array
+                (*x_array)[(*n)][k] = abs_dif;
+                //store 1 to y_array
+                (*y_array)[(*n)]=1;
+                printf("|%s[%d]-%s[%d]| = |%f-%f| = %f  %d\n",outter->path,k,inner->path,k,outter->tfidf[k],inner->tfidf[k],(*x_array)[(*n)][k],(*y_array)[(*n)]);
+            }
+            (*n)++;
+            //reallocate memory in arrays for next difference
+            (*x_array) = realloc((*x_array),((*n)+1)*sizeof(float*));
+            (*y_array) = realloc((*y_array),((*n)+1)*sizeof(unsigned int)); 
+            
+           
+            // Difference between current (outter) and all of the notClique
+           /* if(!revisitedNotClique){
+                while(otherTemp!=NULL){
+                    bucketEntry* other = otherTemp->data; 
+                    //allocate memory in arrays for difference
+                    (*x_array)[(*n)] = calloc(vocabSize,sizeof(float)); //n:= number of absolute differences
+                    for( unsigned int k = 0; k<vocabSize; k++ ){
+                        //calculate difference |outter->tfidf-other->tfidf| 
+                        float abs_dif = abs(outter->tfidf[k]-other->tfidf[k]);
+                        //add to x_array
+                        (*x_array)[(*n)][k] = abs_dif;
+                        //store 0 to y_array
+                        (*y_array)[(*n)]=0;
+                        printf("|%f-%f| = %f  %d\n",outter->tfidf[k],other->tfidf[k],(*x_array)[(*n)][k],(*y_array)[(*n)]);
+                    }
+                    if(otherTemp->next!=NULL){
+                        //reallocate memory in arrays for next difference
+                        (*n)++;
+                        (*x_array) = realloc((*x_array),((*n)+1)*sizeof(float*));
+                        (*y_array) = realloc((*y_array),((*n)+1)*sizeof(unsigned int));
+                    }
+                    otherTemp = otherTemp->next;
+                }
+            }*/
+            innerTemp=innerTemp->next;
+        }
+        outterTemp=outterTemp->next;
+        times++;
+    }
 }
--- Project2/classification/data_for_training.h
+++ Project2/classification/data_for_training.h
@@ -1,3 +1,6 @@
 #include "../hash_table/hash_table.h"
+#include <math.h>
+#include <stdlib.h>
 
-void createTFIDFarray(float*** array,hashTable* ht,unsigned int bucketSize, unsigned int vocabSize,unsigned int* n);
+void createTFIDFarray(float*** x_array,unsigned int** y_array,hashTable* ht,unsigned int bucketSize, unsigned int vocabSize,unsigned int* n);
+void storeAbsDifference(bucketEntry* entryTable_j,float*** x_array,unsigned int** y_array,unsigned int vocabSize,unsigned int* n);
