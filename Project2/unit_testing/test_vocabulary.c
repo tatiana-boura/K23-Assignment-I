01450d3b33bddbc9e9a7c3b8576e389314b74829
Tatiana Boura <tat.boura@gmail.com>
2020-12-17 20:31:07 +0200

    tests for vocabulary and vocab hash -- done

--- Project2/unit_testing/test_vocabulary.c
+++ Project2/unit_testing/test_vocabulary.c
@@ -155,8 +155,209 @@ void test_add_VOC_and_wordInfoList(void){
 
 }
 
+//__TESTERS_FOR_VOCAB_HASH__________________________________________________________
+
+//___test_vocab_hash_create_________________________________________________________
+void test_vocab_hash_create(void){
+
+    hashTableVOC* htVOC = createHTVOC(HTSIZEVOC);
+
+    TEST_ASSERT(htVOC!=NULL);
+    TEST_ASSERT(htVOC->table!=NULL);
+    TEST_ASSERT(htVOC->size==HTSIZEVOC);
+
+	destroyHTVOC(htVOC,BUCKETSIZE);
+
+	return;
+}	
+
+//___test_vocab_hash_addition________________________________________________________
+void test_vocab_hash_add(void){
+
+    hashTableVOC* htVOC = createHTVOC(HTSIZEVOC);
+
+    unsigned int size=0; 
+
+    wordInfo* temp_word_info;
+
+    /*_____________ First sentence / .json ______________________*/
+    char* w1 = calloc(STRSIZE,sizeof(char)); strcpy(w1,"red");
+    char* w2 = calloc(STRSIZE,sizeof(char)); strcpy(w2,"yellow");
+
+    // WordInfoListA gets w1-w2
+    node* wordInfoListA=NULL; 
+    
+    addToVoc(&vocabulary,w1, wordInfoListA,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListA,w1); 
+   
+    addToVoc(&vocabulary,w2,wordInfoListA,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListA,w2); 
+
+    /*_____________ Second sentence / .json ____________________*/
+    char* w3 = calloc(STRSIZE,sizeof(char)); strcpy(w3,"black");
+    char* w4 = calloc(STRSIZE,sizeof(char)); strcpy(w4,"yellow");
+   
+    // WordInfoListB gets w3-w4 
+    node* wordInfoListB=NULL; 
+    
+    addToVoc(&vocabulary,w3,wordInfoListB,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListB,w3); 
+
+    addToVoc(&vocabulary,w4,wordInfoListB,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListB,w4); 
+
+    // START TESTING
+
+    // go through the hash and get the values
+    unsigned int numOfEntries =(BUCKETSIZE-sizeof(bucket*))/sizeof(bucketEntryVOC*);
+
+    for( unsigned int i=0; i<htVOC->size; i++ ){
+
+	    if(htVOC->table[i] != NULL){
+		    node* temp = htVOC->table[i];
+		    while(temp!=NULL){
+		        bucketEntryVOC** entryTable = temp->data;
+		        for(int i = 0;i<numOfEntries;i++){
+		            if((entryTable[i]!=NULL)){
+		            	// check that inserted values are the expected
+		                TEST_ASSERT( !strcmp(entryTable[i]->key,"black") || !strcmp(entryTable[i]->key,"yellow") || !strcmp(entryTable[i]->key,"red") );
+		            }
+		        }
+		        temp=temp->next;
+	   		}
+	    }
+    }
+
+    // free not needed memory
+    destroyListOfWordInfo(wordInfoListA,(void*)wordInfoDeletion);
+    destroyListOfWordInfo(wordInfoListB,(void*)wordInfoDeletion);
+    destroyListOfWordInfo(vocabulary,(void*)wordInfoDeletion);
+    destroyHTVOC(htVOC,BUCKETSIZE);
+
+    return;
+}
+
+//___test_vocab_hash_found__________________________________________________________
+void test_vocab_hash_found(void){
+
+    hashTableVOC* htVOC = createHTVOC(HTSIZEVOC);
+
+    unsigned int size=0; 
+
+    wordInfo* temp_word_info;
+
+    /*_____________ First sentence / .json ______________________*/
+    char* w1 = calloc(STRSIZE,sizeof(char)); strcpy(w1,"red");
+    char* w2 = calloc(STRSIZE,sizeof(char)); strcpy(w2,"yellow");
+
+    // WordInfoListA gets w1-w2
+    node* wordInfoListA=NULL; 
+    
+    addToVoc(&vocabulary,w1, wordInfoListA,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListA,w1); 
+   
+    addToVoc(&vocabulary,w2,wordInfoListA,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListA,w2); 
+
+    /*_____________ Second sentence / .json ____________________*/
+    char* w3 = calloc(STRSIZE,sizeof(char)); strcpy(w3,"black");
+    char* w4 = calloc(STRSIZE,sizeof(char)); strcpy(w4,"yellow");
+   
+    // WordInfoListB gets w3-w4 
+    node* wordInfoListB=NULL; 
+    
+    addToVoc(&vocabulary,w3,wordInfoListB,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListB,w3); 
+
+    addToVoc(&vocabulary,w4,wordInfoListB,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListB,w4); 
+
+    // now check if thw words searched are found
+    bool found;
+
+    // black : [SHOULD BE FOUND]
+    found = foundInHTVOC( htVOC, "black", BUCKETSIZE );
+    TEST_ASSERT( found == true );
+
+    // white : [SHOULD NOT BE FOUND]
+    found = foundInHTVOC( htVOC, "white", BUCKETSIZE );
+    TEST_ASSERT( found == false );
+
+    // free not needed memory
+    destroyListOfWordInfo(wordInfoListA,(void*)wordInfoDeletion);
+    destroyListOfWordInfo(wordInfoListB,(void*)wordInfoDeletion);
+    destroyListOfWordInfo(vocabulary,(void*)wordInfoDeletion);
+    destroyHTVOC(htVOC,BUCKETSIZE);
+
+    return;
+}
+
+//___test_getPositionInAndCountVOC_______________________________________________________
+void test_vocab_hash_getInfo(void){
+
+	// tests the function: getPositionInAndCountVOC()
+
+    hashTableVOC* htVOC = createHTVOC(HTSIZEVOC);
+
+    unsigned int size=0; 
+
+    wordInfo* temp_word_info;
+
+    /*_____________ First sentence / .json ______________________*/
+    char* w1 = calloc(STRSIZE,sizeof(char)); strcpy(w1,"red");
+    char* w2 = calloc(STRSIZE,sizeof(char)); strcpy(w2,"yellow");
+
+    // WordInfoListA gets w1-w2
+    node* wordInfoListA=NULL; 
+    
+    addToVoc(&vocabulary,w1, wordInfoListA,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListA,w1); 
+   
+    addToVoc(&vocabulary,w2,wordInfoListA,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListA,w2); 
+
+    /*_____________ Second sentence / .json ____________________*/
+    char* w3 = calloc(STRSIZE,sizeof(char)); strcpy(w3,"black");
+    char* w4 = calloc(STRSIZE,sizeof(char)); strcpy(w4,"yellow");
+   
+    // WordInfoListB gets w3-w4 
+    node* wordInfoListB=NULL; 
+    
+    addToVoc(&vocabulary,w3,wordInfoListB,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListB,w3); 
+
+    addToVoc(&vocabulary,w4,wordInfoListB,&size,htVOC, BUCKETSIZE);
+    addToWordInfoList(&wordInfoListB,w4); 
+
+    // now check if the the returned data is correct
+    int position;
+    unsigned int count;
+    /* yellow : [POSITION == 1] as it was the second one inserted &&
+				[COUNT == 2]    as it is found in two 'json' */
+    position = getPositionInAndCountVOC( htVOC, "yellow", BUCKETSIZE, &count);
+
+    TEST_ASSERT( position == 1);
+    TEST_ASSERT( count == 2 );
+
+    /* white : [SHOULD NOT BE FOUND -- POSITION == -1] */
+    position = getPositionInAndCountVOC( htVOC, "white", BUCKETSIZE, &count);
+    TEST_ASSERT( position == -1);
+
+    // free not needed memory
+    destroyListOfWordInfo(wordInfoListA,(void*)wordInfoDeletion);
+    destroyListOfWordInfo(wordInfoListB,(void*)wordInfoDeletion);
+    destroyListOfWordInfo(vocabulary,(void*)wordInfoDeletion);
+    destroyHTVOC(htVOC,BUCKETSIZE);
+
+    return;
+
+}
+
 
 TEST_LIST = {
-	{ "test_vocab", test_add_VOC_and_wordInfoList },
+	{ "test_vocab_addition", test_add_VOC_and_wordInfoList },
+	{ "test_vocab_hash_add", test_vocab_hash_add },
+	{ "test_vocab_hash_found", test_vocab_hash_found },
+	{ "test_vocab_hash_getInfo", test_vocab_hash_getInfo },
 	{ NULL, NULL } // end of tests
 };
