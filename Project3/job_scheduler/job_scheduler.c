872eeb329acd8edbd218d310288824e6c7ca81bc
ritachiou <sdi1700192@di.uoa.gr>
2021-01-21 23:13:42 +0200

    Add reading part of threadFunction (arguments to be freed)

--- Project3/job_scheduler/job_scheduler.c
+++ Project3/job_scheduler/job_scheduler.c
@@ -3,24 +3,68 @@
 #include "job_scheduler.h"
 
 typedef struct for_thread{ 
+	circBuffer** cb;
 	pthread_mutex_t* mtx; //pointer to mutex
 	pthread_cond_t* cond; //pointer to condition
+	pthread_mutex_t* wrt;
+	pthread_mutex_t* rd;
 }for_thread;
 
-
+unsigned int active_readers = 0;
 bool ncreated = false ;
 
 void *threadFunction(void* args){
-	printf("locking to mutex[%ld]\n", pthread_self() );
-
 	pthread_mutex_lock(((for_thread*)args)->mtx);
 
 	while(!ncreated){
-		pthread_cond_wait(((for_thread*)args)->cond,((for_thread*)args)->mtx);
+		pthread_cond_wait((((for_thread*)args)->cond),((for_thread*)args)->mtx);
 	}
 	pthread_mutex_unlock(((for_thread*)args)->mtx);
+	pthread_cond_signal(((for_thread*)args)->cond);
 
-	printf("I am thread with id [%ld]. Now I die?\n", pthread_self() );
+	//read job from buffer
+	while(1){
+		pthread_mutex_lock(((for_thread*)args)->mtx);
+		
+		if(!circBufferEmpty(*(((for_thread*)args)->cb))){
+			pthread_mutex_lock(((for_thread*)args)->rd);
+			printf("Reading [%ld]\n",pthread_self());
+			active_readers++;
+			if(active_readers==1){
+				pthread_mutex_lock(((for_thread*)args)->wrt);
+				Batch _data_;
+				bool popped = circBufferPop(*(((for_thread*)args)->cb), &_data_);
+			}
+			pthread_mutex_unlock(((for_thread*)args)->rd);
+			pthread_mutex_lock(((for_thread*)args)->rd);
+			active_readers--;
+			if(active_readers==0)
+				pthread_mutex_unlock(((for_thread*)args)->wrt);
+			pthread_mutex_unlock(((for_thread*)args)->rd);
+			pthread_mutex_unlock(((for_thread*)args)->mtx);
+			//pthread_cond_signal(((for_thread*)args)->cond);
+		}
+		else{
+			//pthread_mutex_unlock(&extra);
+			pthread_mutex_unlock(((for_thread*)args)->mtx);
+			printf("Leaving [%ld]\n",pthread_self());
+			break;
+		}
+	}
+
+	/*while( popped==false){
+		// read one by one from the buffer
+		pthread_mutex_lock(&readFromBuffer);
+		popped = circBufferPop(cbuf, &_data_);
+		pthread_mutex_unlock(&readFromBuffer);
+
+		if( popped==false ){
+			// no entries in the array
+			pthread_mutex_lock(&bufferIsFull);
+		    pthread_cond_wait(&cond, &bufferIsFull);
+		    pthread_mutex_unlock(&bufferIsFull);
+		}
+	}*/
 
 	pthread_exit(NULL);
 	return NULL;
@@ -40,10 +84,18 @@ JobScheduler* initialize_scheduler(int execution_threads){
 	// initialize mutexes and conditions
 	pthread_mutex_init(&(job_sch->mtx), NULL); pthread_cond_init(&(job_sch->cond), NULL);
 	pthread_mutex_init(&(job_sch->wrt), NULL); pthread_mutex_init(&(job_sch->rd), NULL);
+
+	// initialize circ-buffer
+	job_sch->cb = circBufferInit(BUFFSIZE);
+
 	// create arg array
 	for_thread* args = calloc(1,sizeof(for_thread));
 	args->mtx = &(job_sch->mtx);
 	args->cond = &(job_sch->cond);
+	args->rd = &(job_sch->rd);
+	args->wrt = &(job_sch->wrt);
+	//args->cb = calloc(1,sizeof(circBuffer));
+	args->cb = &(job_sch->cb);
 	
 	// create threads and then broadcast
 	pthread_mutex_lock(((for_thread*)args)->mtx); 
@@ -54,15 +106,14 @@ JobScheduler* initialize_scheduler(int execution_threads){
 	//pthread_cond_broadcast(&(((JobScheduler*)job_sch)->cond)); //release the threads
 	pthread_mutex_unlock(((for_thread*)args)->mtx); 
 
-	// initialize circ-buffer
-	job_sch->cb = circBufferInit(BUFFSIZE);
+	
 
 	//wait for all the thread to terminate
 	/*for( unsigned int i=0; i <execution_threads; ++i){
 		pthread_join(job_sch->tids[i], NULL);
 	}*/
 
-	free(args);
+	//free(args); 
 	return job_sch;
 }
 
@@ -72,21 +123,8 @@ int submit_job(JobScheduler* sch, Batch* data){
 	// the buffer itself checks if its full
 	// and does not insert if so
 
-	/*while( popped==false){
-		// read one by one from the buffer
-		pthread_mutex_lock(&readFromBuffer);
-		popped = circBufferPop(cbuf, &_data_);
-		pthread_mutex_unlock(&readFromBuffer);
-
-		if( popped==false ){
-			// no entries in the array
-			pthread_mutex_lock(&bufferIsFull);
-		    pthread_cond_wait(&cond, &bufferIsFull);
-		    pthread_mutex_unlock(&bufferIsFull);
-		}
-	}*/
     pthread_mutex_lock(&(sch->wrt));
-    circBufferInsert(sch->cb,data);
+    circBufferInsert(sch->cb,*data);
     pthread_mutex_unlock(&(sch->wrt));
 
 	//return circBufferInsert(sch->cb,data);
@@ -95,9 +133,10 @@ int submit_job(JobScheduler* sch, Batch* data){
 
 int execute_all_jobs(JobScheduler* sch){
 
-	pthread_mutex_lock(sch->mtx); 
-	pthread_cond_broadcast(&(((JobScheduler*)job_sch)->cond)); //release the threads
-	pthread_mutex_unlock(sch->mtx); 
+	//pthread_mutex_lock(&(sch->mtx)); 
+	//pthread_cond_broadcast(&(((JobScheduler*)sch)->cond)); //release the threads
+	pthread_cond_signal(&(((JobScheduler*)sch)->cond));
+	//pthread_mutex_unlock(&(sch->mtx)); 
 
 	return 1;
 
@@ -105,8 +144,8 @@ int execute_all_jobs(JobScheduler* sch){
 
 int wait_all_tasks_finish(JobScheduler* sch){
 	//wait for all the thread to terminate
-	for( unsigned int i=0; i <execution_threads; ++i){
-		pthread_join(job_sch->tids[i], NULL);
+	for( unsigned int i=0; i <sch->execution_threads; ++i){
+		pthread_join(sch->tids[i], NULL);
 	}
 
 	return 1;
@@ -116,8 +155,7 @@ int wait_all_tasks_finish(JobScheduler* sch){
 void destroy_scheduler(JobScheduler* sch){
 	//destroy mutex & cond
 	pthread_mutex_destroy(&(sch->mtx)); pthread_cond_destroy(&(sch->cond));
-	pthread_mutex_destroy(&(sch->wrt)); pthread_cond_destroy(&(sch->rd));
-	
+	pthread_mutex_destroy(&(sch->wrt)); pthread_mutex_destroy(&(sch->rd));
 	free(sch->tids); sch->tids = NULL;
 	circBufferDealloc(sch->cb);
 	free(sch); sch=NULL;
--- Project3/job_scheduler/sch_tester.c
+++ Project3/job_scheduler/sch_tester.c
@@ -11,6 +11,21 @@ int main(void){
 
 	JobScheduler* js = initialize_scheduler(num_threads);
 
+	Batch* batch1 = calloc(1,sizeof(Batch));
+	batch1->start = 0;
+	batch1->end = 511;
+
+	Batch* batch2 = calloc(1,sizeof(Batch));
+	batch2->start = 512;
+	batch2->end = 1024;
+
+	submit_job(js,batch1);
+	submit_job(js,batch2);
+	
+	execute_all_jobs(js);
+
+	wait_all_tasks_finish(js);
+
 	destroy_scheduler(js);
 
 	return 0;
